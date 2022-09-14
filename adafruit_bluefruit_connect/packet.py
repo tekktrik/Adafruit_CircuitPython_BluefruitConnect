d5db4862c61e6a5b41b4a772c9bf2e470926f086
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-09-13 22:40:39 -0400

    Recover lost packet.py

--- adafruit_bluefruit_connect/packet.py
+++ adafruit_bluefruit_connect/packet.py
@@ -13,7 +13,8 @@ Bluefruit Connect App packet superclass
 """
 
 import struct
-
+import time
+import math
 
 class Packet:
     """
@@ -48,13 +49,22 @@ class Packet:
         to recognize this type of packet.
         """
 
+        print("Ran register")
+        print(cls)
         Packet._type_to_class[cls._TYPE_HEADER] = cls
+
+    def to_bytes(self):
+        raise NotImplementedError("Must be implemented in subclass")
 
     @classmethod
     def from_bytes(cls, packet):
         """Create an appropriate object of the correct class for the given packet bytes.
         Validate packet type, length, and checksum.
         """
+
+        # pylint: disable=import-outside-toplevel
+        from adafruit_bluefruit_connect.image_packet import ImagePacket
+
         if len(packet) < 3:
             raise ValueError("Packet too short")
         packet_class = cls._type_to_class.get(packet[0:2], None)
@@ -66,17 +76,44 @@ class Packet:
         if not issubclass(packet_class, cls):
             raise ValueError("Packet type is not a {}".format(cls.__name__))
 
-        if len(packet) != packet_class.PACKET_LENGTH:
+        if (
+            packet_class != ImagePacket
+            and len(packet) != packet_class.PACKET_LENGTH
+        ):
+            print(packet_class)
             raise ValueError("Wrong length packet")
 
+        #print(cls.checksum(packet[0:-1]))
+        #print("100:", packet[500])
+        #print(packet[-1])
         if cls.checksum(packet[0:-1]) != packet[-1]:
             raise ValueError("Bad checksum")
 
         # A packet class may do further validation of the data.
         return packet_class.parse_private(packet)
 
+
+    def into_stream(self, stream, *, interleave_size=100):
+
+        from adafruit_bluefruit_connect.image_packet import ImagePacket
+
+        data = self.to_bytes()
+
+        if isinstance(self, ImagePacket):
+            stream.write(data[0:7])
+            loop_count = math.ceil(len(data) / interleave_size)
+            slice_start = 0
+            for _ in range(loop_count):
+                slice_end = slice_start + interleave_size
+                stream.write(data[slice_start:slice_end])
+                while stream.read(1) != b"\x06":
+                    pass
+        else:
+            stream.write(data)
+        
+
     @classmethod
-    def from_stream(cls, stream):
+    def from_stream(cls, stream, *, interleave_size=100):
         """Read the next packet from the incoming stream. Wait as long as the timeout
         set on stream, using its own preset timeout.
         Return None if there was no input, otherwise return an instance
@@ -86,16 +123,26 @@ class Packet:
         :param stream stream: an input stream that provides standard stream read operations,
           such as ``ble.UARTServer`` or ``busio.UART``.
         """
+
+        from adafruit_bluefruit_connect.image_packet import ImagePacket
+
         # Loop looking for a b'!' packet start. If the buffer has overflowed,
         # or there's been some other problem, we may need to skip some characters
         # to get to a packet start.
+        print("Ping!")
         while True:
+            print(stream.in_waiting)
             start = stream.read(1)
             if not start:
                 # Timeout: nothing read.
                 return None
+            else:
+                print(start)
+
+            #time.sleep(0.1)
 
             if start == b"!":
+                print("Found packet start!")
                 # Found start of packet.
                 packet_type = stream.read(1)
                 if not packet_type:
@@ -115,9 +162,37 @@ class Packet:
 
         header = bytes(start + packet_type)
         packet_class = cls._type_to_class.get(header, None)
+        print("is get!")
         if not packet_class:
             raise ValueError("Unregistered packet type {}".format(header))
-        packet = header + stream.read(packet_class.PACKET_LENGTH - 2)
+        # TODO: The following will need to be fixed because there is no
+        # PACKET_LENGTH for ImagePacket yet
+        if packet_class == ImagePacket:
+            packet = header
+            colorspace_byte = stream.read(1)
+            colorspace_value = int.from_bytes(colorspace_byte, "little")
+            bytes_per_pixel = 2 if colorspace_value == 16 else 3
+            width_byte = stream.read(2)
+            height_byte = stream.read(2)
+            width_value = int.from_bytes(width_byte, "little")
+            height_value = int.from_bytes(height_byte, "little")
+            size_data = width_value * height_value * bytes_per_pixel + 1
+            packet += colorspace_byte
+            packet += width_byte
+            packet += height_byte
+            print("size_data:", size_data)
+            print("colorspace:", colorspace_value)
+            print("width:", width_value)
+            print("height:", height_value)
+            loop_count = math.ceil(size_data / interleave_size)
+            for _ in range(loop_count):
+                print("ping")
+                stream.read(interleave_size)
+                stream.write(b"\x06")
+            packet += stream.read(size_data)
+            packet += stream.read(1)
+        else:
+            packet = header + stream.read(packet_class.PACKET_LENGTH - 2)
         return cls.from_bytes(packet)
 
     @classmethod
